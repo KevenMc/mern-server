08074ef487fc6ded5d49eb716722fb1ab8a2e931
KevenMc <djeloniticsPASSWORD123>
2023-02-15 20:49:58 +0000

    add UserSchema

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -33,14 +34,20 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
+
+
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
 
 //MONGOOSE SETUP
 const port = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}
-    ).then(() => {
-        app.listen(port, () => console.log(`Server Port : ${port}`))
-    }).catch((error) => console.log(`${error} did not connect`));
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Server Port : ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
